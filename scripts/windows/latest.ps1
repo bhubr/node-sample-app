8c857d4255c92de8c66455a6df61ea9ecc1eb8b4
Benoît Hubert <benoithubert@gmail.com>
2022-06-29 04:41:15 +0200

    Update scripts to push to local registry

--- scripts/windows/latest.ps1
+++ scripts/windows/latest.ps1
@@ -2,9 +2,9 @@
 # build and publish the docker image for a commit to master.
 
 # build and publish the docker image
-docker login -u $env:USERNAME -p $env:PASSWORD
-docker build -t $env:USERNAME/node-sample-app:latest .
-docker push $env:USERNAME/node-sample-app:latest
+# docker login -u $env:USERNAME -p $env:PASSWORD
+docker build -t $env:HOSTNAME:5000/node-sample-app:latest .
+docker push $env:HOSTNAME:5000/node-sample-app:latest
 
 # remove images from local cache
 docker rmi $env:USERNAME/node-sample-app:latest
