9986613a637a3d206086a6156e762bc6a4b83245
Benoît Hubert <benoithubert@gmail.com>
2022-06-29 04:44:34 +0200

    comment and fix build script

--- scripts/windows/latest.ps1
+++ scripts/windows/latest.ps1
@@ -3,8 +3,13 @@
 
 # build and publish the docker image
 # docker login -u $env:USERNAME -p $env:PASSWORD
+
+echo "about to build & push to registry on host (port 5000):"
+echo $env:HOSTNAME
 docker build -t $env:HOSTNAME:5000/node-sample-app:latest .
+echo "build done."
 docker push $env:HOSTNAME:5000/node-sample-app:latest
+echo "image pushed to registry."
 
 # remove images from local cache
-docker rmi $env:USERNAME/node-sample-app:latest
+docker rmi $env:HOSTNAME/node-sample-app:latest
